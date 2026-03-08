cb6fb91fa3480d18b026df263aa0267660a66f7d
shermann94 <66522480+shermann94@users.noreply.github.com>
2026-03-08 20:55:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const supabaseUrl = "https://dmztipmhrwxdjnogznvi.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtenRpcG1ocnd4ZGpub2d6bnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDUxMzMsImV4cCI6MjA4ODUyMTEzM30.yLr4f8NLnLb7Vcf0kTgEMwQXTY8GbAPIZnLRdv3NzzU"
 
-const supabase = window.supabase.createClient(
+const client = supabase.createClient(
 supabaseUrl,
 supabaseKey
 )
@@ -12,7 +12,7 @@ let name = document.getElementById("name").value
 let answer = document.getElementById("answer").value
 let scenario = 1
 
-await supabase
+await client
 .from("answers")
 .insert([
 { name: name, answer: answer, scenario_id: scenario }
